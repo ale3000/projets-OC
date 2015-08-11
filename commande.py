a874406e137af0fec870ee37e58e1a320df3e6a0
ale3000 <alex.bolyn@gmail.com>
2015-08-11 02:36:04 +0200

    Ajout fct Majeur et motif sur init

--- commande.py
+++ commande.py
@@ -1,5 +1,15 @@
 """Ceci est le premier fichier Python (3.x)"""
 class Programme :
 	"""Classe pour notre programme"""
-	def __init__(self):
-		self.nom = "Stuart"
+	def __init__(self,nom,prenom,age):
+		self.nom = nom
+		self.prenom = prenom
+		self.age = age
+	
+	def Majeur(self) :
+		if self.age > 18 and self.< 21 :
+			print("Oui et non... :/")
+		elif self.age > 21 :
+			print("Oui et oui ;)")
+		else :
+			print("non :(")
